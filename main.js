3a2435c8c6521e9a38941cb48ebc1c138c16ac58
Kid48 <21201234a@gmail.com>
2023-10-12 15:54:57 +0200

    continue creating dropdown menu (js)

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
+// Подгрузка footer
+document.addEventListener("DOMContentLoaded", function () {
     fetch('footer.html')
         .then(response => {
             if (!response.ok) {
@@ -13,3 +14,10 @@ document.addEventListener("DOMContentLoaded", function() {
             console.error('Fetch Error: ', error);
         });
 });
+// Dropdown logic
+const menu = document.getElementsByClassName('menu1')[0];
+const hamburger = document.getElementsByClassName('hamburger1')[0];
+
+hamburger.addEventListener('click', function () {
+    menu.classList.toggle("hidden");
+})
