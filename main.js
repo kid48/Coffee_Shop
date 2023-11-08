1cda22353fbfbc3a22b50fa3d84c576cf2c56555
Kid48 <21201234a@gmail.com>
2023-11-08 12:18:49 +0100

    change text color, add margin

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
-// Подгрузка footer
-document.addEventListener("DOMContentLoaded", function () {
-    fetch('footer.html')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok' + response.statusText);
-            }
-            return response.text();
-        })
-        .then(data => {
-            document.getElementById('footerPlaceholder').innerHTML = data;
-        })
-        .catch((error) => {
-            console.error('Fetch Error: ', error);
-        });
-});
-// Dropdown logic
-const menu = document.getElementsByClassName('menu1')[0];
-const hamburger = document.getElementsByClassName('hamburger1')[0];
+// Когда документ загружен и готов, начинается выполнение функций внутри.
+$(document).ready(function() {
 
-hamburger.addEventListener('click', function () {
-    menu.classList.toggle("hidden");
-})
+    // Запрос на получение HTML-содержимого для подвала (footer) страницы.
+    $.ajax({
+        url: 'footer.html', // Указывает путь к файлу, который нужно загрузить.
+        method: 'GET', // Метод запроса HTTP - здесь используется GET для получения данных.
+        success: function(data) {
+            // В случае успешного выполнения запроса содержимое файла 'footer.html'
+            // будет помещено в элемент с id 'footerPlaceholder'.
+            $('#footerPlaceholder').html(data);
+        },
+        error: function(request, status, error) {
+            // В случае ошибки при выполнении запроса в консоль будет выведено сообщение об ошибке.
+            console.error('Fetch Error:', error);
+        }
+    });
+
+    // Логика для выпадающего меню (dropdown menu).
+    // Отслеживание кликов по элементу с классом 'hamburger1'.
+    $('.hamburger1').click(function() {
+        // При клике на элемент с классом 'hamburger1' будет переключаться видимость
+        // меню с классом 'menu1' путем добавления или удаления класса 'hidden'.
+        // Если класс 'hidden' присутствует, меню скрыто. Если отсутствует - меню отображается.
+        $('.menu1').toggleClass('hidden');
+    });
+});
+
