1fa9b138f7a32a8edd43dd438efa19286aeb2e64
Kid48 <21201234a@gmail.com>
2023-12-17 19:02:34 +0100

    add folder components

--- main.js
+++ main.js
@@ -3,12 +3,26 @@ $(document).ready(function() {
 
     // Запрос на получение HTML-содержимого для подвала (footer) страницы.
     $.ajax({
-        url: 'footer.html', // Указывает путь к файлу, который нужно загрузить.
+        url: 'components/footer.html', // Указывает путь к файлу, который нужно загрузить.
         method: 'GET', // Метод запроса HTTP - здесь используется GET для получения данных.
         success: function(data) {
             // В случае успешного выполнения запроса содержимое файла 'footer.html'
             // будет помещено в элемент с id 'footerPlaceholder'.
             $('#footerPlaceholder').html(data);
+        },
+        error: function(request, status, error) {
+            // В случае ошибки при выполнении запроса в консоль будет выведено сообщение об ошибке.
+            console.error('Fetch Error:', error);
+        }
+    });
+
+    $.ajax({
+        url: 'components/header.html', // Указывает путь к файлу, который нужно загрузить.
+        method: 'GET', // Метод запроса HTTP - здесь используется GET для получения данных.
+        success: function(data) {
+            // В случае успешного выполнения запроса содержимое файла 'footer.html'
+            // будет помещено в элемент с id 'footerPlaceholder'.
+            $('#headerPlaceholder').html(data);
         },
         error: function(request, status, error) {
             // В случае ошибки при выполнении запроса в консоль будет выведено сообщение об ошибке.
